41203010ea4cd90310f5bf154e34a09778308c25
HarshMashruwala <harshmashruwala8196@gmail.com>
2022-04-28 08:35:00 +0530

    Bug fixing

--- src/script/ZKPayroll.js
+++ src/script/ZKPayroll.js
@@ -2,7 +2,7 @@ import web3 from './web3_'
 import ZKPayrollAbi from '../abi/ZKPayroll.json'
 
 
-const instance = new web3.eth.Contract(ZKPayrollAbi.abi,'0xc72A8CAaCfd8c71606A33a8086d3C20bA6d69F7e')  
+const instance = new web3.eth.Contract(ZKPayrollAbi.abi,'0x6999bA844230435e15cC31d9251577dAeEb9fD35')  
 // console.log(instance)
 
 export default instance
